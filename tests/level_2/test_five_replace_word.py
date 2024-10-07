897d3c5d3004df5413404a67b68b9f5be93f9532
koolbasov <kolbasov.anton@gmail.com>
2024-10-07 13:53:51 +0300

    level_1 and level_2 with parametrize is ready

--- tests/level_2/test_five_replace_word.py
+++ tests/level_2/test_five_replace_word.py
@@ -2,68 +2,27 @@ import pytest
 from functions.level_2.five_replace_word import replace_word
 
 
-def test__replace_word__different_word():
-    text = "Белка залезла на дерево"
-    replace_from = "белка"
-    replace_to = "Кошка"
-
-    new_text = replace_word(text, replace_from, replace_to)
-
-    assert new_text == "Кошка залезла на дерево"
-
-
-def test__replace_word__same_word():
-    text = "Белка залезла на дерево"
-    replace_from = "Белка"
-    replace_to = "Белка"
-
-    new_text = replace_word(text, replace_from, replace_to)
-
-    assert new_text == "Белка залезла на дерево"
+@pytest.mark.parametrize(
+    "text, replace_from, replace_to, new_text",
+    [
+        ("Белка залезла на дерево", "белка", "Кошка", "Кошка залезла на дерево"),
+        ("Белка залезла на дерево", "Белка", "Белка", "Белка залезла на дерево"),
+        ("Белка залезла на дерево", "", "кошка", "Белка залезла на дерево"),
+        ("Белка залезла на дерево", "Белка", "", " залезла на дерево"),
+    ],
+)
+def test__replace_word__different_cases(text, replace_from, replace_to, new_text):
+    assert replace_word(text, replace_from, replace_to) == new_text
 
 
-def test__replace_word__no_replace_from():
-    text = "Белка залезла на дерево"
-    replace_from = ""
-    replace_to = "кошка"
-
-    new_text = replace_word(text, replace_from, replace_to)
-
-    assert new_text == "Белка залезла на дерево"
-
-
-def test__replace_word__no_replace_to():
-    text = "Белка залезла на дерево"
-    replace_from = "Белка"
-    replace_to = ""
-
-    new_text = replace_word(text, replace_from, replace_to)
-
-    assert new_text == " залезла на дерево"
-
-
-def test__replace_word__wrong_type_text():
-    text = 123
-    replace_from = "Белка"
-    replace_to = "Кошка"
-
-    with pytest.raises(AttributeError):
-        replace_word(text, replace_from, replace_to)
-
-
-def test__replace_word__wrong_type_replace_from():
-    text = "Белка залезла на дерево"
-    replace_from = 123
-    replace_to = "Кошка"
-
-    with pytest.raises(AttributeError):
-        replace_word(text, replace_from, replace_to)
-
-
-def test__replace_word__wrong_type_replace_to():
-    text = "Белка залезла на дерево"
-    replace_from = "Белка"
-    replace_to = 123
-
-    with pytest.raises(TypeError):
+@pytest.mark.parametrize(
+    "text, replace_from, replace_to, expected_exception",
+    [
+        (123, "Белка", "Кошка", AttributeError),
+        ("Белка залезла на дерево", 123, "Кошка", AttributeError),
+        ("Белка залезла на дерево", "Белка", 123, TypeError),
+    ],
+)
+def test__replace_word__exceptions(text, replace_from, replace_to, expected_exception):
+    with pytest.raises(expected_exception):
         replace_word(text, replace_from, replace_to)
